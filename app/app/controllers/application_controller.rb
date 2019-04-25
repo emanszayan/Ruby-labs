5860c112cc3058c53a50ee09dde894f62f87593a
eman <eman.zayan95@gmail.com>
2019-04-25 16:18:15 +0200

    add username

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
     protected
     
         def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+
+            added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+            devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+            devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+
+            #  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+            # devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
         end
 end
